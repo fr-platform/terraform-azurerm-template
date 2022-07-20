45d6d9fd246140c1ece57cf9aa10928258eab80d
OLeonardoRodrigues <oliveira.leonardo.r@gmail.com>
2022-07-20 00:19:33 -0300

    Add provider info to main.tf

--- main.tf
+++ main.tf
@@ -9,4 +9,15 @@
 
 terraform {
   required_version = "~> 1.2.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.93.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
 }
